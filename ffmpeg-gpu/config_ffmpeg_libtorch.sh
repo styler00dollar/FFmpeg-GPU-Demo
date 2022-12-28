5b49d17715eea7fd88c03cd0fa32b55763dd5c6b
sudo <51405565+styler00dollar@users.noreply.github.com>
2022-12-28 03:32:31 +0100

    test

--- ffmpeg-gpu/config_ffmpeg_libtorch.sh
+++ ffmpeg-gpu/config_ffmpeg_libtorch.sh
@@ -8,14 +8,4 @@ TORCH_LIBPATH=$TORCH_ROOT/lib
 --enable-libtensorrt \
 --extra-ldflags=-L$TORCH_LIBPATH \
 --extra-cflags="$TORCH_INCPATH" \
---nvccflags="-gencode=arch=compute_52,code=sm_52 \  
--gencode=arch=compute_60,code=sm_60 \  
--gencode=arch=compute_61,code=sm_61 \  
--gencode=arch=compute_70,code=sm_70 \  
--gencode=arch=compute_75,code=sm_75 \ 
--gencode=arch=compute_80,code=sm_80 \ 
--gencode=arch=compute_86,code=sm_86 \ 
--gencode=arch=compute_87,code=sm_87 \
--gencode=arch=compute_89,code=sm_89 \
--gencode=arch=compute_90,code=sm_90 \ 
--gencode=arch=compute_90,code=compute_90 -lineinfo -Xcompiler -fPIC -I./ $TORCH_INCPATH" 
+--nvccflags="-gencode=arch=compute_89,code=sm_89 -lineinfo -Xcompiler -fPIC -I./ $TORCH_INCPATH" 
